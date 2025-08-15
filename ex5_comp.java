85504aff36c527d46176af0a2d438217227b6b48
Joao Pinheiro De Paula <80402451@pucrs.br>
2025-08-15 13:28:39 -0300

    finish him

--- ex5_comp.java
+++ ex5_comp.java
@@ -5,20 +5,19 @@ public class ex5_comp
 {
     public static void main(String args[]){
     
-        double Tcost = 1000, Nseats = 200;
-        double pro =  30, MinOcup1 = 80, MinOcup2 = 50;
+        double Tcost = 0, Nseats = 0;
+        double pro = 0, MinOcup = 0;
+        
         int show = 0;
         
-        double tic = 0;
+        double ticket = 0;
         
-        double costpro = 0, Useats1 = 0, Useats2= 0;
-        
+        double costpro = 0, Useats = 0;
         
         Scanner in = new Scanner (System.in);
-        
-        for(int i = 0)
-        
-        /*System.out.println("Custo total do show: ");
+
+
+        System.out.println("Custo total do show: ");
         Tcost = in.nextInt();
         
         System.out.println("Numero de poltronas no teatro: ");
@@ -27,29 +26,36 @@ public class ex5_comp
         System.out.println("Percentual de lucro desejado: ");
         pro = in.nextDouble();
         
-        System.out.println("Percentual minimo de ocupacao esperado: ");
-        MinOcup = in.nextDouble();
-        
         System.out.println("Quantidade de espetaculos que serao realizados: ");
-        show = in.nextInt();*/
+        show = in.nextInt();
+
+        int aux = 0;
+
+        for(int i = 1; i <= show; i++){
+            System.out.println("Percentual minimo de ocupacao esperado do show " +i);
+            MinOcup = in.nextDouble();
+            aux += MinOcup;
+        }
+
+        MinOcup = aux/show;
+
+        //System.out.println("media ocup " +MinOcup);
         
         pro /= 100;
-        MinOcup1 /= 100; //ao inves de var = var/100
-        MinOcup2 /= 100; // MinOcup2/100;
+        MinOcup /= 100;
 
         //custo mais o valor de lucro
         costpro = Tcost + (Tcost * pro);
         
         //assentos usados
-        Useats1 = Nseats * MinOcup1;
-        Useats2 = Nseats * MinOcup2;
+        Useats = Nseats * MinOcup;
         
-        System.out.println("seats " + Nseats);
-        System.out.println("valor com pro " + costpro);
+        /*System.out.println("used seats " + Useats);
+        System.out.println("valor com pro " + costpro);*/
         
-        tic = costpro/Nseats;
+        ticket = costpro/Useats;
         
-        System.out.println("valor ticket " +tic);
+        System.out.printf("Valor do ingresso: %.2f%n", ticket);
         
     }
 }
