865234c27f5434d892af28c2ec904e83830fa8bd
Joao Pinheiro De Paula <80402451@pucrs.br>
2025-08-21 17:21:35 -0300

    commit banco

---
+++ lista selecao (switch)/ex4_l3.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class ex4_l3 {
+
+    public static void main(String args[]) {
+        
+        double saldo;
+
+        Scanner in = new Scanner (System.in);
+        
+        System.out.println("Informe o saldo medio da sua conta corrente: ");
+        saldo = in.nextDouble();
+        
+        double limit = 0;
+
+        if(saldo < 500){
+            limit = saldo;
+        }
+        else if (saldo >= 500 && saldo < 1000){
+            limit = saldo * 0.08;
+        }
+        else{
+            limit = saldo * 0.15;
+        }
+
+        int op = 1;
+        double saq = 0, depo = 0;
+        
+        switch (op){
+            case 1:
+                System.out.println("Informe o valor a ser sacado: ");
+                saq = in.nextDouble();
+
+                if(!(saq > 0 && saq <= limit)){
+                    System.out.println("Valor de saque invalido: ");
+                }
+                else{
+                    System.out.println("Saque efetuado! \nSaldo atual: " +(saldo - saq));
+                }
+                break;
+            case 2:
+                System.out.println("Informe o valor a ser depositado: ");
+                depo = in.nextDouble();
+
+                if(depo <= 0){
+                    System.out.println("Valor de deposito invalido: ");                    
+                }
+                else{ 
+                    System.out.println("Deposito efetuado! \nSaldo atual: " +(saldo + depo));
+                    break;
+                }
+            case 3:
+
+                if(limit == saldo){
+                    System.out.println("Sua conta nao possui limite");
+                }
+                else{
+                    System.out.println("Seu limite eh: " + String.format("%.2f", limit));   
+                    break;     
+                }
+            }
+
+        
+    }
+}
