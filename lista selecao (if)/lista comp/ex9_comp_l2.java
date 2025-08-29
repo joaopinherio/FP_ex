6a71fb97819a72ded45e6fffb63fe488cd66f220
João Pinheiro <143855804+joaopinherio@users.noreply.github.com>
2025-08-29 21:51:18 +0000

    aula 29/08

--- lista selecao (if)/lista comp/ex9_comp_l2.java
+++ lista selecao (if)/lista comp/ex9_comp_l2.java
@@ -7,8 +7,6 @@ public class ex9_comp_l2 {
         double num1 = 1, num2 = 2, num3 = 3, num4 = 4, num5 = 5;
 
         Scanner in = new Scanner (System.in);
-
-        //int aux = 0;
     
         num1 = extract_val(num1);
         num2 = extract_val(num2);
@@ -20,18 +18,22 @@ public class ex9_comp_l2 {
         System.out.println("maior numero " + greater(num1, num2, num3, num4, num5));
 
         double menor = lesser(num1, num2, num3, num4, num5);
-        double maior = greater(num1, num2, num3, num4, num5);   
+        double sec_menor = snd_lesser (menor, num1, num2, num3, num4, num5);
+        double maior = greater(num1, num2, num3, num4, num5);
 
-        System.out.println("segundo menor numero" + Snd_lesser(lesser, num1, num2, num3, num4, num5));
+        System.out.println("segundo menor numero " + snd_lesser(menor, num1, num2, num3, num4, num5));
 
+        System.out.println("Media entre os numeros sem o maior e menor" + med_rest(maior, menor, num1, num2, num3, num4, num5));
 
-
-        double result = num1 + num2 + num3 + num4 + num5;
-        result -= maior - menor;
-
-        System.out.println("Media entre os numeros entre o maior e menor" + med(result));
+        System.out.println("Media entre os numeros sem o menor e segundo menor" + med_rest(menor, sec_menor, num1, num2, num3, num4, num5));
 
 
+    }
+
+    public static double med_rest(double G, double L, double n1, double n2, double n3, double n4, double n5){
+        double result = (n1 + n2 + n3 + n4 + n5) - G - L;
+
+        return result /= 3;
     }
 
     public static double extract_val(double a){
@@ -55,15 +57,6 @@ public class ex9_comp_l2 {
         return L;
     }
 
-    public static doulbe snd_lesser(double n1, double n2, double n3, double n4, double n5){
-        
-        
-    
-    }
-
-
-
-
     public static double greater (double n1, double n2, double n3, double n4, double n5){
         double G = n1;
         
@@ -75,24 +68,19 @@ public class ex9_comp_l2 {
         return G;
     }
 
-<<<<<<< HEAD
-    public static double Snd_lesser (double aux, double n1, double n2, double n3, double n4, double n5){
-        double L = aux;
+    public static double snd_lesser (double L, double n1, double n2, double n3, double n4, double n5){
         double L2 = n1;
         
-        if(n2 != L &&  n2 < L2)L2 = n2;
+        if(n1 != L && n1 < L2)L2 = n1;
+        if(n2 != L && n2 < L2)L2 = n2;
         if(n3 != L && n3 < L2)L2 = n3;
         if(n4 != L && n4 < L2)L2 = n4;
         if(n5 != L && n5 < L2)L2 = n5;
-=======
-    public static double med (double a){
-        return a/3;
+
+        return L2;
     }
 
+    
 
 
->>>>>>> refs/remotes/origin/main
-
-        return 2_L;
-    }
 }
