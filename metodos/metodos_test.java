10d5257869cd0bcfa18434561a8ec35ba25b9393
Joao Pinheiro De Paula <80402451@pucrs.br>
2025-09-10 13:31:04 -0300

    1st rf lunch :(

---
+++ metodos/metodos_test.java
@@ -0,0 +1,48 @@
+/*package metodos;
+
+public class metodos_test {
+    public static void main(String args[]){
+
+        double num = 5;
+
+        System.out.println("fatorial: " + fibonacci(num));
+    
+    }
+
+    public static double fatorial(double fat_num){
+        for(double i = fat_num; i > 1; i--){
+            fat_num *= (i - 1);
+        }
+        return fat_num;
+    }
+
+    public static double fibonacci(double fibo_num){
+        return (fibo_num - 1) + (fibo_num - 2);
+    }
+
+
+    public static double lesser (double n1, double n2, double n3, double n4, double n5){
+        double L = n1;
+        
+        if(n2 < L)L = n2;
+        if(n3 < L)L = n3;
+        if(n4 < L)L = n4;
+        if(n5 < L)L = n5;
+
+        return L;
+    }
+
+    public static double greater (double n1, double n2, double n3, double n4, double n5){
+        double G = n1;
+        
+        if(n2 > G)G = n2;
+        if(n3 > G)G = n3;
+        if(n4 > G)G = n4;
+        if(n5 > G)G = n5;
+
+        return G;
+    }
+}*/
+
+
+
